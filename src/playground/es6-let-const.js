c58c2aac64afb06ed7ab1d2166b75e2adcb4ddc5
Rakshith Raj <auanoraj@live.com>
2018-05-05 12:12:32 +0530

    Updated

--- src/playground/es6-let-const.js
+++
@@ -1,8 +0,0 @@
-const multiplier = {
-    numbers: [10, 20, 30],
-    multiplyBy: 5,
-    multiply() {
-    return this.numbers.map((number) => number * this.multiplyBy);
-    }
-}
-console.log(multiplier.multiply());
